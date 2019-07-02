9df74d298233c05ad4cc092d63f192c3100266de
yukaokao <yukaokao@hzxinxinkeji.com>
2019-07-02 16:51:15 +0800

    mod Read/WriteTimeout SetRead/WriteDeadLine

--- network/tcp_conn.go
+++ network/tcp_conn.go
@@ -94,18 +94,16 @@ func (tcpConn *TCPConn) Write(b []byte) {
 	tcpConn.doWrite(b)
 }
 
-func (tcpConn *TCPConn) WriteTimeout(b []byte, d time.Duration) {
-	tcpConn.conn.SetWriteDeadline(time.Now().Add(d))
-	tcpConn.Write(b)
+func (tcpConn *TCPConn) SetWriteDeadline(t time.Time) error {
+	return tcpConn.conn.SetWriteDeadline(t)
 }
 
 func (tcpConn *TCPConn) Read(b []byte) (int, error) {
 	return tcpConn.conn.Read(b)
 }
 
-func (tcpConn *TCPConn) ReadTimeout(b []byte, d time.Duration) (int, error) {
-	tcpConn.conn.SetReadDeadline(time.Now().Add(d))
-	return tcpConn.Read(b)
+func (tcpConn *TCPConn) SetReadDeadline(t time.Time) error {
+	return tcpConn.conn.SetReadDeadline(t)
 }
 
 func (tcpConn *TCPConn) LocalAddr() net.Addr {
